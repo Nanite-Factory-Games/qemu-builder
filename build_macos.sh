e1a6483bc1f231d4ef30e104f222f140dfe9e6ae
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-17 00:00:00 -0400

    Removed arch flag

--- build_macos.sh
+++ build_macos.sh
@@ -2,7 +2,7 @@
 set -ex
 cd ./qemu
 
-./configure --target-list=x86_64-softmmu --cc="clang -arch x86_64" --cxx="clang++ -arch x86_64" || cat /Users/runner/work/qemu-builder/qemu-builder/qemu/build/meson-logs/meson-log.txt
+./configure --target-list=x86_64-softmmu --cc="clang" --cxx="clang++" || cat /Users/runner/work/qemu-builder/qemu-builder/qemu/build/meson-logs/meson-log.txt
 
 # ./configure --target-list=x86_64-softmmu --cc=/usr/bin/clang --host-cc=/usr/bin/clang --ar=/usr/bin/ar --ranlib=/usr/bin/ranlib --strip=/usr/bin/strip --prefix=/usr/local
 # ./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
