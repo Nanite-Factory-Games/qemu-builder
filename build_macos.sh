71a1b7e5fc831f9b6c39f65672333dfc7fb18903
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-16 22:49:34 -0400

    trying out cross compilation on macos

--- build_macos.sh
+++ build_macos.sh
@@ -2,5 +2,6 @@
 set -ex
 cd ./qemu
 
-./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
+./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
+# ./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
 make -j$(sysctl -n hw.physicalcpu)
