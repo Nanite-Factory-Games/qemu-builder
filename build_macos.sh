e774d438ab27cc18b191058cf9b5044d56a72d2d
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-16 23:09:19 -0400

    Updated compiler

--- build_macos.sh
+++ build_macos.sh
@@ -2,6 +2,7 @@
 set -ex
 cd ./qemu
 
-./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
+./configure --target-list=x86_64-softmmu --cc=/usr/bin/clang --host-cc=/usr/bin/clang --ar=/usr/bin/ar --ranlib=/usr/bin/ranlib --strip=/usr/bin/strip --prefix=/usr/local
+# ./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
 # ./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
 make -j$(sysctl -n hw.physicalcpu)
