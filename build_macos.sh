ef43d26725406604e15adf4953c22a9bfe8c31bf
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-16 23:33:14 -0400

    dunno

--- build_macos.sh
+++ build_macos.sh
@@ -2,7 +2,9 @@
 set -ex
 cd ./qemu
 
-./configure --target-list=x86_64-softmmu --cc=/usr/bin/clang --host-cc=/usr/bin/clang --ar=/usr/bin/ar --ranlib=/usr/bin/ranlib --strip=/usr/bin/strip --prefix=/usr/local
+./configure --target-list=x86_64-softmmu --cc="clang -arch x86_64" --cxx="clang++ -arch x86_64" --host-cc="clang -arch x86_64"
+
+# ./configure --target-list=x86_64-softmmu --cc=/usr/bin/clang --host-cc=/usr/bin/clang --ar=/usr/bin/ar --ranlib=/usr/bin/ranlib --strip=/usr/bin/strip --prefix=/usr/local
 # ./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
 # ./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
 make -j$(sysctl -n hw.physicalcpu)
