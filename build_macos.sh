50633bb6602944e0429797d0cb3f3dfbdf93628e
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-17 01:07:39 -0400

    Changed builder tag to one known to use x86-64

--- build_macos.sh
+++ build_macos.sh
@@ -2,13 +2,10 @@
 set -ex
 cd ./qemu
 
-export CFLAGS="-arch x86_64"
-export CXXFLAGS="-arch x86_64"
-export LDFLAGS="-arch x86_64"
 
-./configure --target-list=x86_64-softmmu --cc="clang" --cxx="clang++" || cat /Users/runner/work/qemu-builder/qemu-builder/qemu/build/meson-logs/meson-log.txt
+# ./configure --target-list=x86_64-softmmu --cc="clang" --cxx="clang++" || cat /Users/runner/work/qemu-builder/qemu-builder/qemu/build/meson-logs/meson-log.txt
 
 # ./configure --target-list=x86_64-softmmu --cc=/usr/bin/clang --host-cc=/usr/bin/clang --ar=/usr/bin/ar --ranlib=/usr/bin/ranlib --strip=/usr/bin/strip --prefix=/usr/local
 # ./configure --target-list=x86_64-softmmu --cross-prefix=x86_64-apple-darwin- --cc=/usr/bin/clang --host-cc=/usr/bin/clang
-# ./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
+./configure --target-list=x86_64-softmmu --disable-werror --disable-gtk
 make -j$(sysctl -n hw.physicalcpu)
