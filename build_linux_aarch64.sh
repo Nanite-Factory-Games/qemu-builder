e04f20e08fae239968239d19c60fdcdbc35d0fd0
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-20 03:12:03 -0400

    Removed cross prefix

--- build_linux_aarch64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-cd /mnt/src
-
-./configure --target-list=x86_64-softmmu --cross-prefix=aarch64-linux-gnu- --without-default-features
-make -j$(nproc)
--- build_windows.sh
+++ build_windows.sh
@@ -2,5 +2,5 @@
 set -ex
 cd /mnt/src
 
-./configure --cross-prefix=$1 --target-list=x86_64-softmmu --disable-gtk  --extra-cflags=" -I/usr/x86_64-w64-mingw32/lib" --extra-ldflags="-L/usr/x86_64-w64-mingw32/lib"
+./configure --target-list=x86_64-softmmu --disable-gtk  --extra-cflags=" -I/usr/x86_64-w64-mingw32/lib" --extra-ldflags="-L/usr/x86_64-w64-mingw32/lib"
 make -j$(nproc)
