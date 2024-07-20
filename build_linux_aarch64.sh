6ffd746a19148691823ea9c07866b9d3eea7b30f
Daniel Brenot <daniel.alexander.brenot@gmail.com>
2024-07-19 22:13:17 -0400

    Removed cross compile flag

--- build_linux_aarch64.sh
+++ build_linux_aarch64.sh
@@ -2,5 +2,5 @@
 set -ex
 cd /mnt/src
 
-./configure --target-list=x86_64-softmmu --cross-prefix=aarch64-linux-gnu- --enable-cross-compile --without-default-features
+./configure --target-list=x86_64-softmmu --cross-prefix=aarch64-linux-gnu- --without-default-features
 make -j$(nproc)
